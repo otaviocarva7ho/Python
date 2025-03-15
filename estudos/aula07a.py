a9e7c931b7a74e59b5bee1f9f9f0c5aff21969c1
Otávio Carvalho <carvalhom7otavioh@gmail.com>
2025-03-15 16:48:39 -0300

    Operadores Aritméticos

---
+++ estudos/aula07a.py
@@ -0,0 +1,10 @@
+n1 = int(input("Um Valor: "))
+n2 = int(input("Outro valor: "))
+print(f"A soma vale {n1+n2}")
+print(f"A multiplicação vale {n1*n2}")
+print(f"A Divisão vale {n1/n2}")
+print(f"A Divisão por inteiros vale {n1//n2}")
+print(f"A potência vale {n1**n2}")
+
+# Quebrar linha \n
+# Não quebrar linha end=""
