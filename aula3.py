17936bd5da28283d2a1de7b3dac49f903ce7a2a1
Otávio Carvalho <carvalhom7otavio@gmail.com>
2025-04-20 15:32:05 -0300

    Aula 3

---
+++ aula3.py
@@ -0,0 +1,18 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+# Aspas simples
+print('Otávio Carvalho')
+print('Otávio "Carvalho"')
+# Aspas duplas
+print("Otávio Carvalho")
+print("Otávio 'Carvalho'")
+# Escape
+print("Otávio \"Carvalho\"")
+# r
+print(r"Otávio \"Carvalho\"")
