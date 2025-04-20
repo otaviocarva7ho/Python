f5cde2de967af509b4a26a23b6fb4b58f9bd9924
Otávio Carvalho <carvalhom7otavio@gmail.com>
2025-04-20 14:59:13 -0300

    Aula 1

---
+++ aula1.py
@@ -0,0 +1,17 @@
+"""
+DocString
+E escrever o que eu
+quiser
+
+
+asdfasdfd
+"""
+
+'''
+Usar para escrever um comentário
+'''
+
+# Permite escrever um comentário
+print(123) # Na frente
+# Abaixo
+print(456)
