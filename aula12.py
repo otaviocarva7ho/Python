d327488088ef8a6a9600091191763e35b9d14fdd
Otávio Carvalho <carvalhom7otavio@gmail.com>
2025-04-20 18:52:56 -0300

    Aula 12

---
+++ aula12.py
@@ -0,0 +1,6 @@
+nome = 'Otávio Carvalho'
+altura = 1.75
+peso = 80
+imc = ... # IMC = peso / (altura x altura)
+imc = peso / (altura ** 2)
+print(f'{nome} tem {altura} de altura, pesa {peso} quilos e seu IMC é {imc}')
